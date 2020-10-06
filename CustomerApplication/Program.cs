70b83ccead4ee8357445cb16e32e6e85762fb8f7
laxmi3316 <luxmi.chachan14@gmail.com>
2020-10-06 10:52:07 +0530

    changed message

--- CustomerApplication/Program.cs
+++ CustomerApplication/Program.cs
@@ -6,7 +6,7 @@ namespace CustomerApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi World!");
         }
     }
 }
