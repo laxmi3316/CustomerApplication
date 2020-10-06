313fb666ef3628b7a730ae12d1ea57ee281c222c
laxmi3316 <luxmi.chachan14@gmail.com>
2020-10-06 10:46:59 +0530

    Add project files.

---
+++ CustomerApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CustomerApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
