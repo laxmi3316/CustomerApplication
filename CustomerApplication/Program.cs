66254b0a1a2991646562e6f56f5b4ac9ac3357b3
laxmi3316 <luxmi.chachan14@gmail.com>
2020-10-07 10:33:41 +0530

    project files and test files

--- CustomerApplication/Program.cs
+++ CustomerApplication/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace CustomerApplication
 {
+    delegate void MyCustomdelegate();
+    delegate void mycustomerdelegate(int a);
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hi World!");
+
+            MyCustomdelegate mycustomdel= delegate ()
+            {
+                Console.WriteLine("Inside custom delegate");
+            };
+
+            MyCustomdelegate mycustomdel2 = new MyCustomdelegate(updateapi);
+
+            MyCustomdelegate del = () => Console.WriteLine("Inside custom delegate");
+
+           // mycustomerdelegate del = x => x * x;
+        }
+
+        private static void updateapi()
+        {
+
+
         }
     }
+
+    public class customer
+    {
+        public void AddCustomer()
+        {
+            throw new NotImplementedException();
+        }
+             
+    }
+
 }
---
+++ TestProject/Class1.cs
@@ -0,0 +1,20 @@
+﻿using CustomerApplication;
+using NUnit.Framework;
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace TestProject
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void test()
+        {
+            var test = new customer();
+            Assert.Throws<NotImplementedException>(()=>test.AddCustomer());
+        }
+
+
+    }
+}
